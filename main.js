9f4ef85af43a053a3d8c42ab9d9f4a50ccc38f31
rapp3rs <ralp@inspireteam.com>
2020-02-01 17:02:59 +0800

    create part modules

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ var config = {
 var game = new Phaser.Game(config);
 let player;
 let __monsters;
+let __parts;
 
 //load assets here
 function preload() {
@@ -26,15 +27,18 @@ function preload() {
     this.load.image('bg', 'assets/bg.png');
 
     __monsters = new Monsters(this);
+    __parts = new Parts(this);
 }
 
 //sequence of added images is important. they overlap.
 function create() {
-    this.add.image(530, 450, 'bg');
+    this.add.image(600, 450, 'bg');
     this.physics.add.sprite(400, 300, 'box')
 
     __monsters.initMonsters();
+    __parts.initParts();
 }
 
 function update() {
+
 }
