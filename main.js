0c9d37053b0f6d1dd2248f932303204e562faee2
Clint <cjestal@gmail.com>
2020-02-01 14:41:33 +0800

    moving sprite

--- main.js
+++ main.js
@@ -7,17 +7,18 @@ var config = {
         create,
         update
     },
-      physics: {
-      default: 'arcade',
-      arcade: {
-          gravity: { y: 300 },
-          debug: false
-      }
-  },
+    physics: {
+        default: 'impact',
+        impact: {
+            gravity: 0,
+            debug: false
+        }
+    },
 };
 
 var game = new Phaser.Game(config);
 let player;
+let cursors;
 let __monsters;
 
 //load assets here
@@ -30,11 +31,42 @@ function preload() {
 
 //sequence of added images is important. they overlap.
 function create() {
-    this.add.image(530, 450, 'bg');
-    this.physics.add.sprite(400, 300, 'box')
+    this.add.image(600, 450, 'bg');
+    this.impact.world.setBounds();
+
+    player = this.impact.add.sprite(200, 200, 'bird', 4).setOrigin(0, 0.15);
+    player.setActiveCollision();
+    player.setMaxVelocity(500);
+    player.setFriction(1000, 100);
+    player.body.accelGround = 1200;
+    player.body.accelAir = 600;
+    player.body.jumpSpeed = 500;
+
+    cursors = this.input.keyboard.createCursorKeys();
 
     __monsters.initMonsters();
 }
 
-function update() {
+function update(time, delta) {
+    var accel = player.body.standing ? player.body.accelGround : player.body.accelAir;
+
+    if (cursors.left.isDown) {
+        player.setAccelerationX(-accel);
+    }
+    else if (cursors.right.isDown) {
+        player.setAccelerationX(accel);
+    }
+    else if (cursors.up.isDown) {
+        player.setAccelerationY(-accel);
+    }
+    else if (cursors.down.isDown) {
+        player.setAccelerationY(accel);
+    }
+    else {
+        player.setAccelerationX(0);
+    }
+
+    if (player.vel.x === 0) {
+        player.anims.play('turn');
+    }
 }
