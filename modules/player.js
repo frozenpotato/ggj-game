2e97e4acc2c94fd1f6129b0942e23df21c86b832
Clint <cjestal@gmail.com>
2020-02-02 13:09:20 +0800

    initial pose

--- modules/player.js
+++ modules/player.js
@@ -3,6 +3,7 @@ const SPRITE_ATTRIBS = { frameWidth: 94, frameHeight: 94 }
 class Player {
     constructor(phaserObj) {
         this.phaser = phaserObj;
+        this.phaser.load.spritesheet('player_stand_down', '../assets/player_stand_down.png', SPRITE_ATTRIBS);
         this.phaser.load.spritesheet('player_down', '../assets/player_down.png', SPRITE_ATTRIBS);
         this.phaser.load.spritesheet('player_up', '../assets/player_up.png', SPRITE_ATTRIBS);
         this.phaser.load.spritesheet('player_left', '../assets/player_left.png', SPRITE_ATTRIBS);
@@ -12,6 +13,7 @@ class Player {
         this.phaser.load.spritesheet('player_q3', '../assets/player_q3.png', SPRITE_ATTRIBS);
         this.phaser.load.spritesheet('player_q4', '../assets/player_q4.png', SPRITE_ATTRIBS);
         this.cursors = this.phaser.input.keyboard.createCursorKeys();
+        this.lastDirection = null;
     }
     
     initialize () {
@@ -79,7 +81,7 @@ class Player {
     }
 
     initPlayer() {
-        this.player = this.phaser.physics.add.sprite(200, 200, 'bird', 4).setOrigin(0, 0.15);
+        this.player = this.phaser.physics.add.sprite(400, 100, 'player_stand_down', 4).setOrigin(0, 0.15);
         this.player.setCollideWorldBounds(true);
         this.player.setMaxVelocity(150); //150 if with no item. 50 with item.
         this.player.setFriction(100);
@@ -92,41 +94,50 @@ class Player {
             this.player.setVelocityX(accel);
             this.player.setVelocityY(-accel);
             this.player.anims.play('q1', true);
+            this.lastDirection = 'q1';
         }
         else if (this.cursors.down.isDown && this.cursors.right.isDown) {
             this.player.setVelocityX(accel);
             this.player.setVelocityY(accel);
             this.player.anims.play('q2', true);
+            this.lastDirection = 'q2';
         }
         else if (this.cursors.down.isDown && this.cursors.left.isDown) {
             this.player.setVelocityX(-accel);
             this.player.setVelocityY(accel);
             this.player.anims.play('q3', true);
+            this.lastDirection = 'q3';
         }
         else if (this.cursors.up.isDown && this.cursors.left.isDown){
             this.player.setVelocityX(-accel);
             this.player.setVelocityY(-accel);
             this.player.anims.play('q4', true);
+            this.lastDirection = 'q4';
         }
         else if (this.cursors.left.isDown) {
             this.player.setVelocityX(-accel);
             this.player.anims.play('left', true);
+            this.lastDirection = 'left';
         }
         else if (this.cursors.right.isDown) {
             this.player.setVelocityX(accel);
             this.player.anims.play('right', true);
+            this.lastDirection = 'right';
         }
         else if (this.cursors.up.isDown) {
             this.player.setVelocityY(-accel);
             this.player.anims.play('up', true);
+            this.lastDirection = 'up';
         }
         else if (this.cursors.down.isDown) {
             this.player.setVelocityY(accel);
             this.player.anims.play('down', true);
+            this.lastDirection = 'down';
         }
         else {
             this.player.setVelocityX(0);
             this.player.setVelocityY(0);
+            this.player.loadTexture(`player_stand_${this.lastDirection}`);
         }
     }
 }
